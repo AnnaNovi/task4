09abf31b831579061206f18299b825be6080e260
AnnaNovi <77693820+AnnaNovi@users.noreply.github.com>
2021-10-21 00:05:56 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <v-form />
+  </div>
+</template>
+
+<script>
+import vForm from './components/v-form.vue'
+export default {
+  name: 'App',
+  components: {
+    vForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
